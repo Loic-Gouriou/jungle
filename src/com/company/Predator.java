247fb00e6da66695f695291c3df039f8d78c0292
Loïc <loic.gouriou@edu.itescia.fr>
2020-11-09 14:59:18 +0100

    Ajout des fonctions pour predator et prey

--- src/com/company/Predator.java
+++ src/com/company/Predator.java
@@ -1,4 +1,31 @@
 package com.company;
 
 public class Predator {
+    int Age;
+    int AgeMax;
+    static final int AgeMaxAll = 42;
+
+    int NbLifePoints;
+    int NbLifePointsMax;
+    static final int NbLifePointsAll = 100;
+
+    Smelling smelling;
+    Vision vision;
+
+    public void hunt(){
+
+    }
+    public void mate(){
+
+    }
+    public void eat(){
+
+    }
+    public void drink(){
+
+    }
+    public void move(){
+
+    }
+    
 }
--- src/com/company/Prey.java
+++ src/com/company/Prey.java
@@ -1,4 +1,31 @@
 package com.company;
 
 public class Prey {
+    int Age;
+    int AgeMax;
+    static final int AgeMaxAll = 42;
+
+    int NbLifePoints;
+    int NbLifePointsMax;
+    static final int NbLifePointsAll = 100;
+
+    Hearing hearing;
+    Vision vision;
+
+    public void runAway(){
+        
+    }
+    public void mate(){
+
+    }
+    public void eat(){
+
+    }
+    public void drink(){
+
+    }
+    public void move(){
+
+    }
+
 }
